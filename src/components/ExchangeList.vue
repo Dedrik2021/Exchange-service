e73c9d60415684ad92db4a79ea1f016c9b7f8508
Oleg Grunski <oleggr578@gmail.com>
2023-11-02 13:55:43 +0100

    first commit

---
+++ src/components/ExchangeList.vue
@@ -0,0 +1,71 @@
+<template>
+	<!-- POST LIST START -->
+	<section class="posts">
+		<div class="container">
+			<div class="posts-type">Latest Posts</div>
+			<ul class="columns is-multiline">
+				<li 
+					class="column is-12-mobile is-6-tablet is-4-widescreen is-6-desktop"
+					v-for="item in exchanges"
+					:key="item.id"
+					>
+					<div class="item post-card bottom-border">
+						<a class="item-link" href="#">
+							<figure
+								class="image is-2by1 item-figure background-img"
+								:style="{
+									'background-image':
+										`url(${item.image})`,
+								}"
+								:alt="item.title"
+							></figure>
+							<div class="item-featured">Icon</div>
+						</a>
+						<div class="item-tags">
+							<a 
+								class="button is-rounded" href="#"
+								v-for="tag in item.tags"
+								:key="tag"
+								>
+								#{{tag}}
+							</a>
+						</div>
+						<h2 class="title item-title is-size-4 has-text-weight-extra-bold">
+							<a class="item-link" href="#">{{ item.title }}</a>
+						</h2>
+						<div class="level">
+							<div class="level-left">
+								<div class="item-author">{{ item.user }}</div>
+							</div>
+							<div class="level-right">27th December 2019</div>
+						</div>
+					</div>
+				</li>
+				
+			</ul>
+		</div>
+	</section>
+	<!-- POST LIST END -->
+</template>
+
+<script>
+export default {
+	props: {
+		exchanges: {
+			type: Array,
+			required: true
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.posts {
+	padding-top: 70px;
+}
+.posts-type {
+	font-size: 34px;
+	margin-bottom: 10px;
+	font-weight: bold;
+}
+</style>
---
+++ src/components/NavbarExchange.vue
@@ -0,0 +1,59 @@
+<template>
+	<div>
+		<!-- NAVBAR -->
+		<header class="header">
+			<nav class="navbar">
+				<div class="container">
+					<div class="navbar-brand">
+						<a
+							class="navbar-item has-text-white is-size-2 has-text-weight-bold"
+							href="#"
+						>
+							{{ title }}
+						</a>
+						<span
+							role="button"
+							tabindex="0"
+							class="navbar-burger burger has-text-white"
+							data-target="navbar-menu"
+						>
+							<span></span>
+							<span></span>
+							<span></span>
+						</span>
+					</div>
+					<div id="navbar-menu" class="navbar-menu">
+						<div class="navbar-end">
+							<!-- Loop through the navigation items -->
+							<a
+								class="navbar-item nav-home"
+								v-for="item in items"
+								:href="item.link"
+								:key="item.text"
+							>
+								{{ item.text }}
+							</a>
+						</div>
+					</div>
+				</div>
+			</nav>
+		</header>
+		<!-- NAVBAR END -->
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		title: {
+			type: String,
+			required: true
+		},
+
+		items: {
+			type: Array,
+			required: true
+		},
+	},
+};
+</script>
