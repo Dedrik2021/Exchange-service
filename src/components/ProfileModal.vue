27e6a96c9eb220020662cfb61411ee46d86cc327
Oleg Grunski <oleggr578@gmail.com>
2023-11-09 22:21:45 +0100

    added validator into ProfileModal component

--- src/components/ProfileModal.vue
+++ src/components/ProfileModal.vue
@@ -15,26 +15,32 @@
 						<div class="field">
 							<label class="title">Username</label>
 							<input class="input" v-model="userProfile.username" />
+							<form-errors :errors="v$.userProfile.username.$errors" />
 						</div>
 						<div class="field">
 							<label class="title">Avatar</label>
 							<input class="input" v-model="userProfile.avatar" />
+							<form-errors :errors="v$.userProfile.avatar.$errors" />
 						</div>
 						<div class="field">
 							<label class="title">Info about user</label>
 							<input class="input" v-model="userProfile.info" />
+							<form-errors :errors="v$.userProfile.info.$errors" />
 						</div>
 						<div class="field">
 							<label class="title">Address</label>
 							<input class="input" v-model="userProfile.address" />
+							<form-errors :errors="v$.userProfile.address.$errors" />
 						</div>
 						<div class="field">
 							<label class="title">Country</label>
 							<input class="input" v-model="userProfile.country" />
+							<form-errors :errors="v$.userProfile.country.$errors" />
 						</div>
 						<div class="field">
 							<label class="title">Phone</label>
 							<input class="input" v-model="userProfile.phone" />
+							<form-errors :errors="v$.userProfile.phone.$errors" />
 						</div>
 					</form>
 				</section>
@@ -50,12 +56,26 @@
 </template>
 
 <script>
+import { useVuelidate } from '@vuelidate/core';
+import { required, helpers, numeric, minLength, maxLength } from '@vuelidate/validators';
+
+import FormErrors from './FormErrors.vue';
+import { supportedFileType } from '@/helpers/validators';
+
 export default {
+	components: {
+		FormErrors,
+	},
+
 	props: {
 		user: {
 			type: Object,
 			required: true,
 		},
+	},
+
+	setup() {
+		return { v$: useVuelidate() };
 	},
 
 	data() {
@@ -65,12 +85,57 @@ export default {
 		};
 	},
 
+	validations() {
+		return {
+			userProfile: {
+				username: {
+					required: helpers.withMessage('UserName field cannot be empty!', required),
+				},
+				avatar: {
+					required: helpers.withMessage('Image cannot be empty!', required),
+					supportedFileType: helpers.withMessage(
+						'The Image field value is not a valid URL address!',
+						supportedFileType,
+					),
+				},
+				info: {
+					required: helpers.withMessage('Info field cannot be empty!', required),
+				},
+				address: {
+					required: helpers.withMessage('Address field cannot be empty!', required),
+				},
+				country: {
+					required: helpers.withMessage('Country field cannot be empty!', required),
+				},
+				phone: {
+					required: helpers.withMessage('Phone field cannot be empty!', required),
+					numeric: helpers.withMessage(
+						'Phone field must be contain number signs!',
+						numeric,
+					),
+					maxLength: helpers.withMessage(
+						'Phone field must be contain max 9 number characters!',
+						maxLength(9),
+					),
+					minLength: helpers.withMessage(
+						'Phone field must be contain min 9 number characters!',
+						minLength(9),
+					),
+				},
+			},
+		};
+	},
+
 	methods: {
-		updateProfile() {
-			this.$store.dispatch('user/updateProfile', {
-				data: this.userProfile,
-				onSuccess: () => (this.isOpen = false),
-			});
+		async updateProfile() {
+			const isValid = await this.v$.$validate();
+
+			if (isValid) {
+				this.$store.dispatch('user/updateProfile', {
+					data: this.userProfile,
+					onSuccess: () => (this.isOpen = false),
+				});
+			}
 		},
 	},
 };
